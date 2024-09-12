1e68ab711427d9a475d1b1ec8c1c7bf8817ef1d1
DanStaf <danstaf@mail.ru>
2024-09-12 11:42:34 +0300

    Create team, program (status of hero, parent)

--- heroes/admin.py
+++ heroes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from heroes.models import Parent, Hero, Team, Program
 
 # Register your models here.
+
+admin.site.register(Parent)
+admin.site.register(Hero)
+admin.site.register(Team)
+admin.site.register(Program)
